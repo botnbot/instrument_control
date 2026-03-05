d4d4334a04209125751eee6dfe615f27ff22bf61
Sergey Korobeynikov <skoro@smn35.com>
2026-03-05 21:43:35 +0300

    Добавлена STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -82,13 +82,9 @@ DATABASES = {
 }
 
 
-
-
-
-
-
-
 # Password validation
+
+
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -120,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
