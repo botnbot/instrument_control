a0c1271d761cf47ee53ed81fc0996f21bb3c1093
Sergey Korobeynikov <skoro@smn35.com>
2026-03-08 11:01:46 +0300

    highlight.py created in core\templatetags

---
+++ core/templatetags/highlight.py
@@ -0,0 +1,18 @@
+from django import template
+from django.utils.safestring import mark_safe
+import re
+
+register = template.Library()
+
+@register.filter(name='highlight')
+def highlight(text, search):
+    if not search:
+        return text
+
+    pattern = re.compile(re.escape(search), re.IGNORECASE)
+    highlighted = pattern.sub(
+        lambda m: f'<mark>{m.group(0)}</mark>',
+        str(text)
+    )
+
+    return mark_safe(highlighted)
