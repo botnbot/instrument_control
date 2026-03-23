212926d90a7542c15e4e7823ee855ec30b63a266
Sergey Korobeynikov <skoro@smn35.com>
2026-03-23 13:00:35 +0300

    code formatted

--- core/forms.py
+++ core/forms.py
@@ -1,28 +1,27 @@
 from django import forms
 
-from core.models import Instruments, Repairs, Repairers,  Relocations
+from core.models import Instruments, Repairs, Repairers, Relocations
 
 
 class InstrumentsForm(forms.ModelForm):
     class Meta:
         model = Instruments
-        fields = '__all__'
+        fields = "__all__"
 
 
 class RepairersForm(forms.ModelForm):
     class Meta:
         model = Repairers
-        fields = '__all__'
+        fields = "__all__"
 
 
 class RepairsForm(forms.ModelForm):
     class Meta:
         model = Repairs
-        fields = '__all__'
+        fields = "__all__"
 
 
 class RelocationsForm(forms.ModelForm):
     class Meta:
         model = Relocations
-        fields = '__all__'
-
+        fields = "__all__"
--- core/templatetags/highlight.py
+++ core/templatetags/highlight.py
@@ -4,15 +4,13 @@ import re
 
 register = template.Library()
 
-@register.filter(name='highlight')
+
+@register.filter(name="highlight")
 def highlight(text, search):
     if not search:
         return text
 
     pattern = re.compile(re.escape(search), re.IGNORECASE)
-    highlighted = pattern.sub(
-        lambda m: f'<mark>{m.group(0)}</mark>',
-        str(text)
-    )
+    highlighted = pattern.sub(lambda m: f"<mark>{m.group(0)}</mark>", str(text))
 
     return mark_safe(highlighted)
