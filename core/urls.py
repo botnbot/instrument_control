a65f9a435975595d2f40d5a0f59d3d691ec3c006
Sergey Korobeynikov <skoro@smn35.com>
2026-03-23 13:47:19 +0300

    code formatted

--- core/urls.py
+++ core/urls.py
@@ -18,19 +18,16 @@ from django.urls import path
 
 from core.views import (
     HomeView,
-
     InstrumentsListView,
     InstrumentsCreateView,
     InstrumentsDetailView,
     InstrumentsUpdateView,
     InstrumentsDeleteView,
-
     RepairersCreateView,
     RepairersListView,
     RepairersDetailView,
     RepairersUpdateView,
     RepairersDeleteView,
-
     RepairsCreateView,
     RepairsListView,
     RepairsDetailView,
@@ -39,27 +36,46 @@ from core.views import (
     SendForRepairView,
 )
 
-app_name = 'core'
+app_name = "core"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-
-    path('instruments/', InstrumentsListView.as_view(), name='instrument_list'),
-    path('instruments/create/', InstrumentsCreateView.as_view(), name='instrument_create'),
-    path('instruments/<int:pk>/', InstrumentsDetailView.as_view(), name='instrument_detail'),
-    path('instruments/<int:pk>/update/', InstrumentsUpdateView.as_view(), name='instrument_edit'),
-    path('instruments/<int:pk>/delete/', InstrumentsDeleteView.as_view(), name='instrument_delete'),
-
-    path('repairers/', RepairersListView.as_view(), name='repairer_list'),
-    path('repairers/create/', RepairersCreateView.as_view(), name='repairer_create'),
-    path('repairers/<int:pk>/', RepairersDetailView.as_view(), name='repairer_detail'),
-    path('repairers/<int:pk>/update/', RepairersUpdateView.as_view(), name='repairer_update'),
-    path('repairers/<int:pk>/delete/', RepairersDeleteView.as_view(), name='repairer_delete'),
-
-    path('repairs/', RepairsListView.as_view(), name='repair_list'),
-    path('repairs/create/', RepairsCreateView.as_view(), name='repair_create'),
-    path('repairs/<int:pk>/', RepairsDetailView.as_view(), name='repair_detail'),
-    path('repairs/<int:pk>/update/', RepairsUpdateView.as_view(), name='repair_update'),
-    path('repairs/<int:pk>/delete/', RepairsDeleteView.as_view(), name='repair_delete'),
-    path('repairs/<int:pk>/send/', SendForRepairView.as_view(), name='repair_send'),
+    path("", HomeView.as_view(), name="home"),
+    path("instruments/", InstrumentsListView.as_view(), name="instrument_list"),
+    path(
+        "instruments/create/", InstrumentsCreateView.as_view(), name="instrument_create"
+    ),
+    path(
+        "instruments/<int:pk>/",
+        InstrumentsDetailView.as_view(),
+        name="instrument_detail",
+    ),
+    path(
+        "instruments/<int:pk>/update/",
+        InstrumentsUpdateView.as_view(),
+        name="instrument_edit",
+    ),
+    path(
+        "instruments/<int:pk>/delete/",
+        InstrumentsDeleteView.as_view(),
+        name="instrument_delete",
+    ),
+    path("repairers/", RepairersListView.as_view(), name="repairer_list"),
+    path("repairers/create/", RepairersCreateView.as_view(), name="repairer_create"),
+    path("repairers/<int:pk>/", RepairersDetailView.as_view(), name="repairer_detail"),
+    path(
+        "repairers/<int:pk>/update/",
+        RepairersUpdateView.as_view(),
+        name="repairer_update",
+    ),
+    path(
+        "repairers/<int:pk>/delete/",
+        RepairersDeleteView.as_view(),
+        name="repairer_delete",
+    ),
+    path("repairs/", RepairsListView.as_view(), name="repair_list"),
+    path("repairs/create/", RepairsCreateView.as_view(), name="repair_create"),
+    path("repairs/<int:pk>/", RepairsDetailView.as_view(), name="repair_detail"),
+    path("repairs/<int:pk>/update/", RepairsUpdateView.as_view(), name="repair_update"),
+    path("repairs/<int:pk>/delete/", RepairsDeleteView.as_view(), name="repair_delete"),
+    path("repairs/<int:pk>/send/", SendForRepairView.as_view(), name="repair_send"),
 ]
--- user/urls.py
+++ user/urls.py
@@ -19,13 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'user'
+app_name = "user"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',  include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
-
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
