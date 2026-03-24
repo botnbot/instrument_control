5050acd2c434564348ccee339a4e9904129e090c
Sergey Korobeynikov <skoro@smn35.com>
2026-03-25 00:57:34 +0300

    class User some changes

--- users/models.py
+++ users/models.py
@@ -1,15 +1,23 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager, Group, Permission
 from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20)
-    email = models.EmailField()
-    is_active = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ["first_name", "last_name"]
-
-    objects = UserManager()
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='custom_user_set',
+        help_text='Группы, к которым принадлежит пользователь.'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='custom_user_permissions_set',
+        help_text='Специальные права для этого пользователя.'
+    )
 
     def __str__(self):
-        return f"{self.first_name}_{self.last_name}"
+        return self.username or self.name
