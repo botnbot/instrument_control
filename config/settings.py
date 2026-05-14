6f653ea15ad953836e4edb47ef56cff7b44152e2
Sergey Korobeynikov <skoro@smn35.com>
2026-05-14 14:47:30 +0300

    Variables are retrieved from the .env file.

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,11 @@
+from datetime import timedelta
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = "django-insecure-x#+)0i2@oiy071^pecuv*l)focx7e_@&nc-9_+yl*vdfbsuyew"
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev')
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web', 'nginx']
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -11,12 +13,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
     "users",
+    'core.apps.CoreConfig',
     'widget_tweaks',
     'rest_framework',
-    'simplejwt'
-
+    'rest_framework_simplejwt',
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -28,6 +29,23 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 ROOT_URLCONF = "config.urls"
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Требовать авторизацию для API
+    ],
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,  # Давать новый refresh при каждом обновлении токена
+    'BLACKLIST_AFTER_ROTATION': True, # Старый refresh сразу запрещать
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY, # Используем стандартный SECRET_KEY
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -44,13 +62,13 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "instrument_db",
-        "USER": "instrument_user",
-        "PASSWORD": "12345",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'instrument_db'),
+        'USER': os.environ.get('DB_USER', 'instrument_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '12345'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
@@ -71,10 +89,9 @@ LANGUAGE_CODE = "ru"
 TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
+STATIC_ROOT = BASE_DIR / 'static_collected'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 AUTH_USER_MODEL = "users.User"
